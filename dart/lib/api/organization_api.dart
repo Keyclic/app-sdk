b8c63efdd8f81f639a171ff50e2f27b35507bcca
marin <marin.virlogeux@gmail.com>
2022-03-02 10:26:23 +0100

    place occupants -> organization occupants

--- dart/lib/api/organization_api.dart
+++ dart/lib/api/organization_api.dart
@@ -502,6 +502,117 @@ class OrganizationApi {
         as MemberPagination;
   }
 
+  /// Retrieve all Occupant resources.
+  ///
+  ///
+  Future<OccupantPagination> cgetOccupantsByOrganization(
+    String xKeyclicApp,
+    String organization, {
+    String acceptLanguage,
+    DateTime xDateTime,
+    String xKeyclicAppPlatform,
+    String xKeyclicAppVersion,
+    List<String> order__,
+    DateTime after,
+    DateTime before,
+    String member,
+    List<String> members__,
+    String person,
+    List<String> persons__,
+    String place,
+    List<String> places__,
+    String query,
+    int page,
+    int limit,
+  }) async {
+    // verify required params are set
+
+    if (xKeyclicApp == null) {
+      throw ApiException(0, "Missing required param: xKeyclicApp");
+    }
+
+    if (organization == null) {
+      throw ApiException(0, "Missing required param: organization");
+    }
+
+    // create path and map variables
+    final String path = "/organizations/{organization}/occupants"
+        .replaceAll("{format}", "json")
+        .replaceAll("{" + "organization" + "}", organization.toString());
+
+    // query params
+    final List<QueryParam> queryParams = <QueryParam>[
+      if (order__ != null)
+        ..._convertParametersForCollectionFormat("order[]", order__,
+            collectionFormat: "multi"),
+      if (after != null)
+        ..._convertParametersForCollectionFormat("after", after),
+      if (before != null)
+        ..._convertParametersForCollectionFormat("before", before),
+      if (member != null)
+        ..._convertParametersForCollectionFormat("member", member),
+      if (members__ != null)
+        ..._convertParametersForCollectionFormat("members[]", members__,
+            collectionFormat: "multi"),
+      if (person != null)
+        ..._convertParametersForCollectionFormat("person", person),
+      if (persons__ != null)
+        ..._convertParametersForCollectionFormat("persons[]", persons__,
+            collectionFormat: "multi"),
+      if (place != null)
+        ..._convertParametersForCollectionFormat("place", place),
+      if (places__ != null)
+        ..._convertParametersForCollectionFormat("places[]", places__,
+            collectionFormat: "multi"),
+      if (query != null)
+        ..._convertParametersForCollectionFormat("query", query),
+      if (page != null) ..._convertParametersForCollectionFormat("page", page),
+      if (limit != null)
+        ..._convertParametersForCollectionFormat("limit", limit),
+    ];
+
+    // header params
+    final Map<String, String> headerParams = <String, String>{
+      if (acceptLanguage is String)
+        "accept-language": acceptLanguage.toString(),
+      if (xDateTime is DateTime) "x-date-time": xDateTime.toIso8601String(),
+      if (xKeyclicApp is String) "x-keyclic-app": xKeyclicApp.toString(),
+      if (xKeyclicAppPlatform is String)
+        "x-keyclic-app-platform": xKeyclicAppPlatform.toString(),
+      if (xKeyclicAppVersion is String)
+        "x-keyclic-app-version": xKeyclicAppVersion.toString(),
+    };
+
+    final List<String> contentTypes = <String>[
+      "application/json;charset=UTF-8",
+      "application/json",
+    ];
+
+    final List<String> authNames = <String>[
+      "bearer",
+    ];
+
+    final Response response = await apiClient.invokeAPI(
+      path: path,
+      method: 'GET',
+      queryParams: queryParams,
+      headerParams: headerParams,
+      contentType: contentTypes[0],
+      authNames: authNames,
+    );
+
+    if (response.statusCode >= 400) {
+      throw ApiException(response.statusCode, response.body);
+    }
+
+    if (response.body == null) {
+      return null;
+    }
+
+    return apiClient.deserialize(response.body, 'OccupantPagination')
+        as OccupantPagination;
+  }
+
   /// Retrieve all Operation resources.
   ///
   ///

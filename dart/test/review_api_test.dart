43adb2f9f13a2c7890b75213c67a06f529b84651
marin <mvirlogeux@twipi-group.com>
2024-10-10 12:17:28 +0200

    chore/deprecated-api-v1

--- dart/test/review_api_test.dart
+++ dart/test/review_api_test.dart
@@ -10,11 +10,6 @@ void main() {
   // final instance = ReviewApi();
 
   group('tests for ReviewApi', () {
-    // Retrieve one Review resource.
-    //
-    //Future<Review> getReview(String xKeyclicApp, String review, { String acceptLanguage, DateTime xDateTime, String xKeyclicAppPlatform, String xKeyclicAppVersion }) async
-    test('test getReview', () async {});
-
     // Create one Review resource.
     //
     //Future<Review> postReview(String xKeyclicApp, ReviewData reviewData, { String acceptLanguage, DateTime xDateTime, String xKeyclicAppPlatform, String xKeyclicAppVersion }) async

0f311c14145e679e55d38dc22881d68771a80a87
marin <mvirlogeux@twipi-group.com>
2024-11-25 16:59:18 +0100

    chore/move-bookmark-get-endpoint

---
+++ dart-v3/doc/BookmarkJsonhalReadEmbedded.md
@@ -0,0 +1,16 @@
+# keyclic_sdk_api_platform.model.BookmarkJsonhalReadEmbedded
+
+## Load the model package
+```dart
+import 'package:keyclic_sdk_api_platform/api.dart';
+```
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**place** | [**AssetJsonhalRead**](AssetJsonhalRead.md) |  | [optional] 
+**member** | [**MemberJsonhalRead**](MemberJsonhalRead.md) |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ dart-v3/test/get_bookmark_collection200_response_test.dart
@@ -0,0 +1,25 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+// import 'package:keyclic_sdk_api_platform/api.dart';
+import 'package:test/test.dart';
+
+// tests for GetBookmarkCollection200Response
+void main() {
+  // final instance = GetBookmarkCollection200Response();
+
+  group('test GetBookmarkCollection200Response', () {
+    // GetBookmarkCollection200ResponseEmbedded embedded
+    test('to test the property `embedded`', () async {});
+
+    // int totalItems
+    test('to test the property `totalItems`', () async {});
+
+    // int itemsPerPage
+    test('to test the property `itemsPerPage`', () async {});
+
+    // GetBookmarkCollection200ResponseLinks links
+    test('to test the property `links`', () async {});
+  });
+}

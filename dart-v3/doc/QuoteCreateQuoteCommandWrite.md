a0079747065d2b16b5f8041aaa609e1d23785bae
Mehdi <mkhalilelouadghiri@twipi-group.com>
2025-01-30 11:52:39 +0100

    Merge pull request #3137

---
+++ dart-v3/doc/QuoteCreateQuoteCommandWrite.md
@@ -0,0 +1,32 @@
+# keyclic_sdk_api_platform.model.QuoteCreateQuoteCommandWrite
+
+## Load the model package
+```dart
+import 'package:keyclic_sdk_api_platform/api.dart';
+```
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**amountExcludingTax** | [**PriceWrite**](PriceWrite.md) |  | 
+**amountIncludingTax** | [**PriceWrite**](PriceWrite.md) |  | 
+**documents** | [**Object**](.md) |  | [optional] 
+**equipments** | [**Object**](.md) |  | [optional] 
+**expirationDate** | [**DateTime**](DateTime.md) |  | [optional] 
+**issueDate** | [**DateTime**](DateTime.md) |  | 
+**member** | **String** |  | [optional] 
+**memberContactPoint** | [**ContactPointWrite**](ContactPointWrite.md) |  | [optional] 
+**name** | **String** |  | 
+**number** | **String** |  | 
+**origin** | [**Object**](.md) |  | [optional] 
+**place** | **String** |  | 
+**provider** | **String** |  | [optional] 
+**providerAddress** | [**PostalAddressWrite**](PostalAddressWrite.md) |  | [optional] 
+**providerContactPoint** | [**ContactPointWrite**](ContactPointWrite.md) |  | [optional] 
+**resolutionDate** | [**DateTime**](DateTime.md) |  | [optional] 
+**state** | **String** |  | [optional] 
+**task** | **String** |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ dart-v3/test/quote_create_quote_command_write_test.dart
@@ -0,0 +1,70 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+// import 'package:keyclic_sdk_api_platform/api.dart';
+import 'package:test/test.dart';
+
+// tests for QuoteCreateQuoteCommandWrite
+void main() {
+  // final instance = QuoteCreateQuoteCommandWrite();
+
+  group('test QuoteCreateQuoteCommandWrite', () {
+    // PriceWrite amountExcludingTax
+    test('to test the property `amountExcludingTax`', () async {});
+
+    // PriceWrite amountIncludingTax
+    test('to test the property `amountIncludingTax`', () async {});
+
+    //
+    // Map<String, dynamic> documents
+    test('to test the property `documents`', () async {});
+
+    //
+    // Map<String, dynamic> equipments
+    test('to test the property `equipments`', () async {});
+
+    // DateTime expirationDate
+    test('to test the property `expirationDate`', () async {});
+
+    // DateTime issueDate
+    test('to test the property `issueDate`', () async {});
+
+    // String member
+    test('to test the property `member`', () async {});
+
+    // ContactPointWrite memberContactPoint
+    test('to test the property `memberContactPoint`', () async {});
+
+    // String name
+    test('to test the property `name`', () async {});
+
+    // String number
+    test('to test the property `number`', () async {});
+
+    //
+    // Map<String, dynamic> origin
+    test('to test the property `origin`', () async {});
+
+    // String place
+    test('to test the property `place`', () async {});
+
+    // String provider
+    test('to test the property `provider`', () async {});
+
+    // PostalAddressWrite providerAddress
+    test('to test the property `providerAddress`', () async {});
+
+    // ContactPointWrite providerContactPoint
+    test('to test the property `providerContactPoint`', () async {});
+
+    // DateTime resolutionDate
+    test('to test the property `resolutionDate`', () async {});
+
+    // String state
+    test('to test the property `state`', () async {});
+
+    // String task
+    test('to test the property `task`', () async {});
+  });
+}
